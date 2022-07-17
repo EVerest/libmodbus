31a8b00e8d5905a706f527a9cc3f3f537da75174
Christoph Kliemt <christoph.kliemt@pionix.de>
2022-07-17 17:55:13 +0200

    rtu: refactored a bit

--- tests/test_rtu.cpp
+++ tests/test_rtu.cpp
@@ -76,39 +76,31 @@ TEST(RTUTests, test_crc16 ) {
 
 int openSerialDevice( const std::string device ) {
 
-    using namespace std::string_literals;
+    // using namespace std::string_literals;
 
     int fd = open( device.c_str() , O_RDWR );
 
     if ( not ( fd == -1 ) )
         return fd;
 
-    throw std::runtime_error( "Error open serial device: "s + device + " Reason: " + strerror( errno ) );
+    throw std::runtime_error( "Error open serial device: " + device + " Reason: " + strerror( errno ) );
 
 }
 
-// void configureSerialDevice(
+void getCurrentSerialDeviceConfiguration( int, serial_port_fd, termios* tty ) {
 
-TEST(RTUTests, test_lowlevel_serial ) {
-
-    // int serial_port = open("/dev/ttyUSB0", O_RDWR);
-    // int serial_port = open("/dev/tty3", O_RDWR);
-
-    ASSERT_THROW( openSerialDevice("/dev/does_not_exist" ), std::runtime_error );
-
-    int serial_port = openSerialDevice("/dev/ttyUSB0" ); // throws std::runtime_error if open fails
-
-    // configureSerialDevice( serial_port, tty_config, options );
-
-    termios tty;
-
-    int tcgetattr_result = tcgetattr(serial_port, &tty);
-    if( tcgetattr_result != 0 ) {
-        printf("Error %i from tcgetattr: %s\n", errno, strerror(errno));
+    if( tcgetattr(serial_port_fd, tty) != 0 ) {
+        int myerror = errno;
+        throw std::runtime_error( "Error " + std::to_string( myerror ) + " from tcgetattr: " + strerror(myerror));
     }
 
-    ASSERT_EQ( tcgetattr_result, 0 );
+}
 
+void configureSerialDevice( int serial_port_fd, termios* tty /*, const SerialDeviceOptions& */ ) {
+
+    // using namespace std::string_literals;
+
+    // we are a bit verbose with these settings...
     // BSM power meter used this as default:
     // 19200 Baud
     cfsetspeed(&tty, B19200);
@@ -128,7 +120,6 @@ TEST(RTUTests, test_lowlevel_serial ) {
     // disable canonical mode (in canonical mode, data is processed after newline).
     tty.c_lflag &= ~ICANON;
 
-    //
     tty.c_lflag &= ~ECHO; // Disable echo
     tty.c_lflag &= ~ECHOE; // Disable erasure
     tty.c_lflag &= ~ECHONL; // Disable new-line echo
@@ -143,19 +134,29 @@ TEST(RTUTests, test_lowlevel_serial ) {
    // VMIN = 0, VTIME = 0: No blocking, return immediately with what is available
    // play with this... we will see if this is needed.
    // VMIN is a character count ranging from 0 to 255 characters, and VTIME is time measured in 0.1 second intervals, (0 to 25.5 seconds).
-    tty.c_cc[VTIME] = 50; // wait at most for 5 seconds.
+   tty.c_cc[VTIME] = 50; // wait at most for 5 seconds.
    tty.c_cc[VMIN]  = 0;
 
    // Save tty settings, also checking for error
-   int tcsetattr_result = tcsetattr(serial_port, TCSANOW, &tty);
-   if ( tcsetattr_result != 0) {
-       printf("Error %i from tcsetattr: %s\n", errno, strerror(errno));
+   if ( tcsetattr(serial_port, TCSANOW, &tty) != 0) {
+       int myerror = errno;
+       throw std::runtime_error("Error " + std::to_string( myerror ) + " from tcsetattr: " + strerror( myerror ));
    }
 
-   ASSERT_EQ( tcsetattr_result , 0 );
+}
 
+TEST(RTUTests, test_lowlevel_serial ) {
+
+    ASSERT_THROW( openSerialDevice("/dev/does_not_exist" ), std::runtime_error );
+
+    // PLAN: this will be moved into a method in the connection object
+    int serial_port = openSerialDevice("/dev/ttyUSB0" ); // throws std::runtime_error if open fails
+    termios tty_config;
+    getCurrentSerialDeviceConfiguration( serial_port, &tty_config );
+    configureSerialDevice( serial_port, &tty_config /*, options */ );
+
+    // Test starts here.
    const unsigned char request_common_model[] { 0x2A,0x03,0x9C,0x44,0x00,0x42,0xAD,0xA5 };
-   // const unsigned char request_common_model[] { 0x2A,0x03,0x9C,0x44,0x00,0x42,0x00,0x00 };
 
    int bytes_written = write( serial_port, request_common_model , sizeof(request_common_model)/sizeof(unsigned char) );
 
